c686fdd412f3f55e6a0db18c5cb889abd1c9ec6f
Mostafa_Alabraqi <mostafaalabraqi@gmail.com>
2025-10-04 17:12:45 +0300

    add note method to add note cubit

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -5,11 +5,11 @@ sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
 
-final class AddNoteLoadung extends AddNoteState {}
+final class AddNoteLoading extends AddNoteState {}
 final class AddNoteSuccess extends AddNoteState {}
 final class AddNoteFailure extends AddNoteState {
 
   final String errMessage;
-  
+
   AddNoteFailure(this.errMessage);
 }
