c8d49d88b3c6edbbacbbd8fb996b9a129dea1fe8
Mostafa_Alabraqi <mostafaalabraqi@gmail.com>
2025-10-22 02:08:08 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -7,13 +7,3 @@ final class NotesInitial extends NotesState {}
 
 final class NotesLoading extends NotesState {}
 
-final class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccess(this.notes);
-}
-
-final class NotesFailure extends NotesState {
-  final String errMessage;
-  NotesFailure(this.errMessage);
-}
